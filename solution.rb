0c81b9c26fca0d157fa447ed0aa460f3aaac61dd
Miguel Casas <miguel@kommit.co>
2015-12-24 19:58:03 -0500

    abuela sorda

--- solution.rb
+++ solution.rb
@@ -1,7 +1,19 @@
 require "sinatra"
-use Rack::Session::Pool, :expire_after => 2592000
+
+enable :sessions
 
 get '/' do
-  @valor=session[:valor] = (session[:valor].to_i + 1).to_s
   erb:form
+end
+
+post '/response' do
+  if params[:data].match(/[^A-Z]/) 
+    <<-HTML
+     <h1>habla mas durito mijo</h1>
+     HTML
+  else
+    <<-HTML
+     <h1>Ahh si , manzanas</h1>
+     HTML
+  end
 end
