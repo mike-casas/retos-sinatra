fd9e43b28d7959e2e69acc06bc718e3a91f820b4
Miguel Casas <miguel@kommit.co>
2015-12-24 19:55:50 -0500

    edit rack

--- solution.rb
+++ solution.rb
@@ -1,10 +1,5 @@
 require "sinatra"
-
-use Rack::Session::Cookie, :key => 'rack.session',
-                           :path => '/',
-                           :expire_after => 2592000,
-                           :secret => 'change_me',
-                           :old_secret => 'also_change_me'
+use Rack::Session::Pool, :expire_after => 2592000
 
 get '/' do
   @valor=session[:valor] = (session[:valor].to_i + 1).to_s
