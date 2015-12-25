f1721fb2082fe43d88b9c5d224a971594a94f930
Miguel Casas <miguel@kommit.co>
2015-12-24 20:18:32 -0500

    resolve  sum number

--- solution.rb
+++ solution.rb
@@ -1,5 +1,13 @@
 require "sinatra"
 
+use Rack::Session::Pool, :expire_after => 2592000
+
 get '/' do
-request.env['HTTP_USER_AGENT']
+
+  erb:form
+end
+
+post '/' do
+  @valor=session[:valor] = (session[:valor].to_i + 1).to_s
+  redirect  '/'
 end
