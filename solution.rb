7147d850aa2601be7f40c82ba1db2bae455e9fdb
Miguel Casas <miguel@kommit.co>
2015-12-24 20:11:04 -0500

    what is my browser

--- solution.rb
+++ solution.rb
@@ -1,17 +1,5 @@
 require "sinatra"
 
 get '/' do
-  erb:form
-end
-
-post '/response' do
-  if params[:data].match(/[^A-Z]/) 
-    <<-HTML
-     <h1>Habla más duro mijito</h1>
-     HTML
-  else
-    <<-HTML
-     <h1>Ahhh si, manzanas!</h1>
-     HTML
-  end
+request.env['HTTP_USER_AGENT']
 end
